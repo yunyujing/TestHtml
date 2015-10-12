59a055d286b9ec7202a28f4b60e03b6309bac489
yunyujing <15136271194@163.com>
2015-10-12 16:14:28 +0800

    url的不同加载情况

--- app/src/main/java/com/example/testhtml/MainActivity.java
+++ app/src/main/java/com/example/testhtml/MainActivity.java
@@ -4,34 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private WebSettings webSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void init() {
+        webView= (WebView) findViewById(R.id.welcome_webView);
+        webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.requestFocus();
+        //加载指定url的html界面
+//        webView.loadUrl("http://www.baidu.com");
+        //加载asset文件中的的html界面
+        webView.loadUrl("file:///android_asset/html/welcome.html");
+        //加载本地sd卡中的html界面
+//        webView.loadUrl("content://com.android.appshare/sdcard/aaa.htm");
+
+        //使用webView显示浏览的网页
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(url!=null){
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
